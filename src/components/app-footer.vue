4ae15a4dc459e98d3bf588980130c216f69d86de
Martien Dermawan Tanama <martiendt@gmail.com>
2025-03-24 09:14:08 +0700

    x

---
+++ src/components/app-footer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  version: string
+}>()
+</script>
+
+<template>
+  <div class="flex justify-between px-4">
+    <p>Pointhub &copy; 2025</p>
+    <p class="self-end">v 0.1.1</p>
+  </div>
+</template>
